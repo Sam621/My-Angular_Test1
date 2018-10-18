b8cbfd76428641d4c5d624098bfc1b04450e17e6
GEOMATIC\ssemasundara <ssemasundara@geomatic.com.au>
2018-10-18 12:24:09 +1100

    first commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
