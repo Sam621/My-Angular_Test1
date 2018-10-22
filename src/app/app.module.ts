7c36a71f83f75c9bebc0c9793ced72f236225afd
Sanjaya Semasundara <ssemasundara@geomatic.com.au>
2018-10-22 17:28:57 +1100

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '*', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     LoginComponent,
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
